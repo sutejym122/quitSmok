aeaae875a67e81a30923fa3533a32a547560ac2e
sutej <sutejym122@gmail.com>
2026-07-24 15:26:58 -0400

    Initial Commit

---
+++ BUILT/BUILTApp.swift
@@ -0,0 +1,17 @@
+//
+//  BUILTApp.swift
+//  BUILT
+//
+//  Created by Sutej  Ym  on 7/24/26.
+//
+
+import SwiftUI
+
+@main
+struct BUILTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BUILTTests/BUILTTests.swift
@@ -0,0 +1,17 @@
+//
+//  BUILTTests.swift
+//  BUILTTests
+//
+//  Created by Sutej  Ym  on 7/24/26.
+//
+
+import Testing
+@testable import BUILT
+
+struct BUILTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
